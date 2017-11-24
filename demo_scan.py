a888c8711d429da029c32a1db47f121fccb30a3c
Josh Bressers <josh@bress.net>
2017-11-24 16:23:59 -0600

    Add a demo script to write data into elasticsearch

---
+++ demo_scan.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from cybertestlab import Analysis
+import json
+import sys
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(['http://localhost:9200'])
+
+a = Analysis.Analysis('/usr/bin', debug=True)
+
+elfs = a.find_elfs()
+b = a.scan_elfs(elfs)
+
+for i in b.keys():
+    try:
+        es.update(id=i, index="ctl", doc_type='doc', body={'doc' :b[i], 'doc_as_upsert': True})
+    except:
+        # We'll figure this out someday
+        pass
+
