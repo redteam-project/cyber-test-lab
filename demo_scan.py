82735d831c3f84ae2be168ea94185b862327bfcf
Josh Bressers <josh@bress.net>
2017-11-24 17:32:56 -0600

    Update elasticsearch constantly instead of at the end

--- demo_scan.py
+++ demo_scan.py
@@ -13,12 +13,16 @@ else:
     a = Analysis.Analysis('/usr/bin', debug=True)
 
 elfs = a.find_elfs()
-b = a.scan_elfs(elfs)
 
-for i in b.keys():
-    try:
-        es.update(id=i, index="ctl", doc_type='doc', body={'doc' :b[i], 'doc_as_upsert': True})
-    except:
-        # We'll figure this out someday
-        pass
+# Let's loop this so we don't chew up all the RAM
+for one_elf in elfs:
+
+    b = a.scan_elfs([one_elf])
+
+    for i in b.keys():
+        try:
+            es.update(id=i, index="ctl", doc_type='doc', body={'doc' :b[i], 'doc_as_upsert': True})
+        except:
+            # We'll figure this out someday
+            pass
 
