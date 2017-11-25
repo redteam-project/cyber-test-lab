8b4c5b2f1ce96d3ebaa70ec6b1e6f9b350285584
Josh Bressers <josh@bress.net>
2017-11-24 18:41:20 -0600

    Fix the tests

--- cybertestlab/tests/test_Analysis.py
+++ cybertestlab/tests/test_Analysis.py
@@ -11,13 +11,11 @@ def test_find_elfs():
 def test_scan_elfs():
     a = Analysis.Analysis('/usr/bin')
 
-    elfs = a.find_elfs()
-    elfs = [elfs[1]]
-    b = a.scan_elfs(elfs)
-    assert b.has_key('complexity')
+    b = a.scan_elfs(['/usr/bin/yes'])
+    assert b.has_key('yes')
 
 def test_get_complexity():
 
     a = Analysis.Analysis('/usr/bin')
     b = a.get_complexity('/usr/bin/yes')
-    assert b.has_key('r2 aa')
+    assert b.has_key('r2aa')
