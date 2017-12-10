66bd3ba82fd7dc20edec88038c507e7d380ab6d1
Jason Callaway <jason@jasoncallaway.com>
2017-12-10 17:48:22 -0500

    started scoring work, incremented version numbers

--- cybertestlab/CyberTestLab.py
+++ cybertestlab/CyberTestLab.py
@@ -11,7 +11,7 @@ from redteam import redteam
 __author__ = 'Jason Callaway'
 __email__ = 'jasoncallaway@fedoraproject.org'
 __license__ = 'GNU Public License v2'
-__version__ = '0.1'
+__version__ = '0.2'
 __status__ = 'alpha'
 
 
