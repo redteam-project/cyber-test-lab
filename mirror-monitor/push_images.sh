3cc599ed4b7c0c6d5b71a82ecef51e61b4f8b02a
Jason Callaway <jasoncallaway@google.com>
2020-02-02 21:22:14 -0500

    added script to push local docker images to gcr

---
+++ mirror-monitor/push_images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker tag ctl-centos6:latest gcr.io/red-team-project/ctl-centos6:latest; docker push gcr.io/red-team-project/ctl-centos6:latest
+docker tag ctl-centos7:latest gcr.io/red-team-project/ctl-centos7:latest; docker push gcr.io/red-team-project/ctl-centos7:latest
+docker tag ctl-centos8:latest gcr.io/red-team-project/ctl-centos8:latest; docker push gcr.io/red-team-project/ctl-centos8:latest
+docker tag ctl-debian8:latest gcr.io/red-team-project/ctl-debian8:latest; docker push gcr.io/red-team-project/ctl-debian8:latest
+docker tag ctl-debian9:latest gcr.io/red-team-project/ctl-debian9:latest; docker push gcr.io/red-team-project/ctl-debian9:latest
+docker tag ctl-ubuntu1604:latest gcr.io/red-team-project/ctl-ubuntu1604:latest; docker push gcr.io/red-team-project/ctl-ubuntu1604:latest
+docker tag ctl-ubuntu1804:latest gcr.io/red-team-project/ctl-ubuntu1804:latest; docker push gcr.io/red-team-project/ctl-ubuntu1804:latest
+docker tag ctl-fedora26:latest gcr.io/red-team-project/ctl-fedora26:latest; docker push gcr.io/red-team-project/ctl-fedora26:latest
+docker tag ctl-fedora27:latest gcr.io/red-team-project/ctl-fedora27:latest; docker push gcr.io/red-team-project/ctl-fedora27:latest
+docker tag ctl-fedora28:latest gcr.io/red-team-project/ctl-fedora28:latest; docker push gcr.io/red-team-project/ctl-fedora28:latest
+docker tag ctl-fedora29:latest gcr.io/red-team-project/ctl-fedora29:latest; docker push gcr.io/red-team-project/ctl-fedora29:latest
+docker tag ctl-fedora30:latest gcr.io/red-team-project/ctl-fedora30:latest; docker push gcr.io/red-team-project/ctl-fedora30:latest
+docker tag ctl-fedora31:latest gcr.io/red-team-project/ctl-fedora31:latest; docker push gcr.io/red-team-project/ctl-fedora31:latest
+docker tag ctl-archlinux:latest gcr.io/red-team-project/ctl-archlinux:latest; docker push gcr.io/red-team-project/ctl-archlinux:latest
