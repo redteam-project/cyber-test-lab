b5556a146397fb0d71b86b707c9d981609c7c9e8
Jason Callaway <jasoncallaway@google.com>
2020-02-02 20:53:18 -0500

    added fedora mirror file updaters

---
+++ mirror-monitor/docker/fedora/26/update_mirrors.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dnf update -y
+gcloud auth activate-service-account --key-file=/secrets/creds.json
+gsutil cp -r /etc/yum.repos.d gs://${MIRROR_BUCKET}/mirrors/files/${MIRROR_OS}/${MIRROR_VERSION}/
