50e49c038f449f6f08ae46d6c203d85d1077ba6a
Jason Callaway <jasoncallaway@google.com>
2020-02-02 19:53:07 -0500

    added centos 8 mirror updater

---
+++ mirror-monitor/docker/centos/8/update_mirrors.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+yum update -y
+gcloud auth activate-service-account --key-file=/secrets/creds.json
+gsutil cp -r /etc/yum.repos.d gs://${MIRROR_BUCKET}/mirrors/files/${MIRROR_OS}/${MIRROR_VERSION}/
