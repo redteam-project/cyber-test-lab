7c37a82d76fdf00506299b1c127d483d74566ccc
Jason Callaway <jasoncallaway@google.com>
2020-02-02 19:41:27 -0500

    added epel to centos 6 mirror updater. added centos 7 mirror updater.

--- mirror-monitor/docker/centos/7/update_mirrors.sh
+++ mirror-monitor/docker/centos/7/update_mirrors.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 yum update -y
-source /opt/rh/rh-python35/enable
 gcloud auth activate-service-account --key-file=/secrets/creds.json
 gsutil cp -r /etc/yum.repos.d gs://${MIRROR_BUCKET}/mirrors/files/${MIRROR_OS}/${MIRROR_VERSION}/
-gsutil cp /var/cache/yum/x86_64/6/timedhosts.txt gs://${MIRROR_BUCKET}/mirrors/files/${MIRROR_OS}/${MIRROR_VERSION}/
+gsutil cp /var/cache/yum/x86_64/7/timedhosts.txt gs://${MIRROR_BUCKET}/mirrors/files/${MIRROR_OS}/${MIRROR_VERSION}/
