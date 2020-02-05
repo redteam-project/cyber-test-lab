bfd7a4fc0630c82fab56b47a0d7b7ba6b7a0db76
Jason Callaway <jasoncallaway@google.com>
2020-02-05 15:20:21 -0500

    added fix for gsutil cp on nonexistent files

--- mirror-monitor/docker/ubuntu/1604/update_mirrors.sh
+++ mirror-monitor/docker/ubuntu/1604/update_mirrors.sh
@@ -4,4 +4,6 @@ apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 gcloud auth activate-service-account --key-file=/secrets/creds.json
 gsutil cp /etc/apt/sources.list gs://${MIRROR_BUCKET}/mirrors/files/${MIRROR_OS}/${MIRROR_VERSION}/
-gsutil cp -r /etc/apt/sources.list.d gs://${MIRROR_BUCKET}/mirrors/files/${MIRROR_OS}/${MIRROR_VERSION}/
+if [ "${?}" -eq "0" ]; then
+  gsutil cp -r /etc/apt/sources.list.d gs://${MIRROR_BUCKET}/mirrors/files/${MIRROR_OS}/${MIRROR_VERSION}/
+fi
