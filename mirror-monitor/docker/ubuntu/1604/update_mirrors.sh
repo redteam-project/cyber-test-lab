1e668498f6b75c7c52804189f01a1074d0bf9ba7
Jason Callaway <jasoncallaway@google.com>
2020-02-06 13:22:52 -0500

    added missing file checks to ubuntu mirror update scripts

--- mirror-monitor/docker/ubuntu/1604/update_mirrors.sh
+++ mirror-monitor/docker/ubuntu/1604/update_mirrors.sh
@@ -5,6 +5,7 @@ apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 gcloud auth activate-service-account --key-file=/secrets/creds.json
 gsutil cp /etc/apt/sources.list gs://${MIRROR_BUCKET}/mirrors/files/${MIRROR_OS}/${MIRROR_VERSION}/
+ls -A /etc/apt/sources.list.d/* >/dev/null 2>&1
 if [ "${?}" -eq "0" ]; then
   gsutil cp -r /etc/apt/sources.list.d gs://${MIRROR_BUCKET}/mirrors/files/${MIRROR_OS}/${MIRROR_VERSION}/
 fi
