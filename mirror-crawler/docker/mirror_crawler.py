1890cb6c67ffdd58fd532700c203bd6b806384f2
Jason Callaway <jason@jasoncallaway.com>
2020-02-15 10:18:19 -0500

    added geters for blobs and files

--- mirror-crawler/docker/mirror_crawler.py
+++ mirror-crawler/docker/mirror_crawler.py
@@ -1,10 +1,28 @@
+import os
+
+from google.cloud import bigquery
+from google.cloud import storage
+
 
 class MirrorCrawler(object):
-
   def __init__(self):
-    pass
+    self.config = {
+      'bucket_name': os.environ.get('BUCKET_NAME'),
+      'bucket_path': os.environ.get('BUCKET_PATH'),
+      'mirror_files_path': os.environ.get('MIRROR_FILES_PATH')
+    }
+    self.storage_client = storage.Client()
 
+  def get_blobs(self, path) -> list:
+    bucket_name = self.config['bucket_name'] + '/' + self.config[path]
+    blobs = self.storage_client.list_blobs(bucket_name)
+    return blobs
 
+  def get_file(self, path, filename, local_path):
+    bucket_name = self.config['bucket_name'] + '/' + self.config[path]
+    bucket = self.storage_client.bucket(bucket_name)
+    blob = bucket.blob(filename)
+    blob.download_to_filename(local_path)
 
 def main():
   pass
